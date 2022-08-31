18ac61ce668cf08a7627a454534a71dcadb5bf34
israel <israelrezendeluiz2903@gmail.com>
2022-08-31 11:30:54 -0300

    first commit, iniciado layout de agencia de viagens.

---
+++ app/src/main/java/rezende/israel/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,16 @@
+package rezende.israel.aluraviagens.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import rezende.israel.aluraviagens.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
