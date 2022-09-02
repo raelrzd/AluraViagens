6f6c3ad0233bba075bb869ab7b4e666f292bb262
israel <israelrezendeluiz2903@gmail.com>
2022-09-02 15:06:43 -0300

    incluido novo arquivo de layout e realizada suas configurações iniciais.

---
+++ app/src/main/java/rezende/israel/aluraviagens/ui/activity/ResumoPacoteActivity.java
@@ -0,0 +1,18 @@
+package rezende.israel.aluraviagens.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import rezende.israel.aluraviagens.R;
+
+public class ResumoPacoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumo_pacote);
+
+
+    }
+}
