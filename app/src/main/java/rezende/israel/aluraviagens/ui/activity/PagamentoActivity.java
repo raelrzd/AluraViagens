5de1116589c5e3d9b1461aa6668aedcd731e9bf8
israel <israelrezendeluiz2903@gmail.com>
2022-09-05 16:11:02 -0300

    incluido novo arquivo de layout referente ao Resumo da compra, configurações iniciais realizdas.

--- app/src/main/java/rezende/israel/aluraviagens/ui/activity/PagamentoActivity.java
+++ app/src/main/java/rezende/israel/aluraviagens/ui/activity/PagamentoActivity.java
@@ -1,5 +1,6 @@
 package rezende.israel.aluraviagens.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -23,6 +24,9 @@ public class PagamentoActivity extends AppCompatActivity {
 
         Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
         mostraPreco(pacoteSaoPaulo);
+
+        Intent intent = new Intent(this, ResumoCompraActivity.class);
+        startActivity(intent);
 
     }
 
---
+++ app/src/main/java/rezende/israel/aluraviagens/ui/activity/ResumoCompraActivity.java
@@ -0,0 +1,17 @@
+package rezende.israel.aluraviagens.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import rezende.israel.aluraviagens.R;
+
+public class ResumoCompraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumo_compra);
+        setTitle("Resumo Compra");
+    }
+}
