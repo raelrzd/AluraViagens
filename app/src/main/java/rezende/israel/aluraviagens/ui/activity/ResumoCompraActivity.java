bf73e45771bfc75ced58fa6b44e7d5f3d9687162
israel <israelrezendeluiz2903@gmail.com>
2022-09-05 17:50:55 -0300

    realizada a refatoração activity e arquivo xml do layout de Resumo da compra.

--- app/src/main/java/rezende/israel/aluraviagens/ui/activity/ResumoCompraActivity.java
+++ app/src/main/java/rezende/israel/aluraviagens/ui/activity/ResumoCompraActivity.java
@@ -2,7 +2,6 @@ package rezende.israel.aluraviagens.ui.activity;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,28 +17,43 @@ import rezende.israel.aluraviagens.util.ResourceUtil;
 
 public class ResumoCompraActivity extends AppCompatActivity {
 
+    public static final String TITULO_APPBAR = "Resumo Compra";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumo_compra);
-        setTitle("Resumo Compra");
+        setTitle(TITULO_APPBAR);
 
         Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
 
-        ImageView imagem = findViewById(R.id.resumo_compra_imagem_local);
-        Drawable drawable = ResourceUtil.devolveDrawable(this, pacoteSaoPaulo.getImagem());
-        imagem.setImageDrawable(drawable);
+        mostraImagem(pacoteSaoPaulo);
+        mostraLocal(pacoteSaoPaulo);
+        mostraData(pacoteSaoPaulo);
+        mostraPreco(pacoteSaoPaulo);
 
-        TextView local = findViewById(R.id.resumo_compra_local);
-        local.setText(pacoteSaoPaulo.getLocal());
+    }
 
-        TextView data = findViewById(R.id.resumo_compra_data);
-        String dataFormatada = DataUtil.periodoEmTexto(pacoteSaoPaulo.getDias());
-        data.setText(dataFormatada);
-
+    private void mostraPreco(Pacote pacote) {
         TextView preco = findViewById(R.id.resumo_compra_preco);
-        String moedaBr = MoedaUtil.formataParaMoedaBr(pacoteSaoPaulo.getPreco());
+        String moedaBr = MoedaUtil.formataParaMoedaBr(pacote.getPreco());
         preco.setText(moedaBr);
+    }
 
+    private void mostraData(Pacote pacote) {
+        TextView data = findViewById(R.id.resumo_compra_data);
+        String dataFormatada = DataUtil.periodoEmTexto(pacote.getDias());
+        data.setText(dataFormatada);
+    }
+
+    private void mostraLocal(Pacote pacote) {
+        TextView local = findViewById(R.id.resumo_compra_local);
+        local.setText(pacote.getLocal());
+    }
+
+    private void mostraImagem(Pacote pacote) {
+        ImageView imagem = findViewById(R.id.resumo_compra_imagem_local);
+        Drawable drawable = ResourceUtil.devolveDrawable(this, pacote.getImagem());
+        imagem.setImageDrawable(drawable);
     }
 }
