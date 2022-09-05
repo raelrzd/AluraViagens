6ce39f90c3d09974cb66c74eea6468add6c98af3
israel <israelrezendeluiz2903@gmail.com>
2022-09-05 14:45:19 -0300

    incluido parametro de filtro dos caracteres possiveis no nome do cartão, e tambem feito o Bind da informação do preco do pacote da tela de pagamento.

--- app/src/main/java/rezende/israel/aluraviagens/ui/activity/PagamentoActivity.java
+++ app/src/main/java/rezende/israel/aluraviagens/ui/activity/PagamentoActivity.java
@@ -1,17 +1,34 @@
 package rezende.israel.aluraviagens.ui.activity;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.math.BigDecimal;
 
 import rezende.israel.aluraviagens.R;
+import rezende.israel.aluraviagens.model.Pacote;
+import rezende.israel.aluraviagens.util.MoedaUtil;
 
 public class PagamentoActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Pagamento";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagamento);
-        setTitle("Pagamento");
+        setTitle(TITULO_APPBAR);
+
+        Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
+        mostraPreco(pacoteSaoPaulo);
+
+    }
+
+    private void mostraPreco(Pacote pacote) {
+        TextView preco = findViewById(R.id.pagamento_preco_pacote);
+        String moedaBr = MoedaUtil.formataParaMoedaBr(pacote.getPreco());
+        preco.setText(moedaBr);
     }
 }
